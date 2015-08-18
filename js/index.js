586d18cff09401a973e2bc29f6cf019fed6e78e1
jlgunden <jlgunden@gmail.com>
2015-08-17 20:26:28 -0400

    bunch of misc items

--- js/index.js
+++ js/index.js
@@ -1,10 +1,10 @@
 
 $(document).ready(function() {
   $(document.body).fadeIn(1500);
-  var mobile_menu = new slide_out();
-  mobile_menu.decorate($('#sliding_menu'));
+  var navigation_drawer = new nav_drawer();
+  navigation_drawer.decorate($('#sliding_menu'));
   
   $('#menu').click(function() {
-    mobile_menu.trigger_show();
+    navigation_drawer.trigger_show();
   })
 });
--- sass/config.rb
+++ sass/config.rb
@@ -3,10 +3,10 @@ require 'compass/import-once/activate'
 
 # Set this to the root of your project when deployed:
 http_path = "/"
-css_dir = "css"
-sass_dir = "sass"
-images_dir = "images"
-javascripts_dir = "js"
+css_dir = "../css"
+sass_dir = "."
+images_dir = "../images"
+javascripts_dir = "../javascripts"
 
 # You can select your preferred output style here (can be overridden via the command line):
 # output_style = :expanded or :nested or :compact or :compressed
@@ -16,7 +16,7 @@ output_style = :compact
 # relative_assets = true
 
 # To disable debugging comments that display the original location of your selectors. Uncomment:
-# line_comments = false
+line_comments = false
 
 
 # If you prefer the indented syntax, you might want to regenerate this
