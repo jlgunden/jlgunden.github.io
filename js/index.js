5dbb311211284f4fa795658786a18b04bdf4930f
jlgunden <jlgunden@gmail.com>
2015-08-19 07:34:10 -0400

    adding new lines to end of files

--- js/index.js
+++ js/index.js
@@ -3,7 +3,7 @@ $(document).ready(function() {
   $(document.body).fadeIn(1500);
   var navigation_drawer = new nav_drawer();
   navigation_drawer.decorate($('#sliding_menu'));
-  
+
   $('#menu').click(function() {
     navigation_drawer.trigger_show();
   })
