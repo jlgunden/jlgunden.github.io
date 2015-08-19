08f6a816360ba4b35d17d0d41bdb4f82eff43b40
jlgunden <jlgunden@gmail.com>
2015-08-18 22:23:34 -0400

    nav drawer menu items

--- js/nav_drawer.js
+++ js/nav_drawer.js
@@ -5,7 +5,25 @@
  * @return {void}
  */
 var nav_drawer = function() {
-  this.items = ['Home', 'About', 'Resume', 'Contact'];
+  this.items_order = ['Home', 'About', 'Resume', 'Contact'];
+  this.items = {
+    'Home': {
+      'name': 'Home',
+      'link': 'index.html'
+    },
+    'About': {
+      'name': 'About',
+      'link': 'about.html'
+    },
+    'Resume': {
+      'name': 'Resume',
+      'link': 'resume.html'
+    },
+    'Contact': {
+      'name': 'Contact',
+      'link': 'contact.html'
+    }
+  };
 };
 
 /**
@@ -19,12 +37,18 @@ nav_drawer.prototype.decorate = function(parent)  {
   this.menu = $(document.createElement('div'))
     .addClass('nav_drawer');
 
-  for (var i = 0; i < this.items.length; i++) {
+  var ul = $(document.createElement('ul'));
+
+  for (var i = 0; i < this.items_order.length; i++) {
     this.menu.append(
-      $(document.createElement('div'))
+      $(document.createElement('li'))
         .addClass('nav_drawer_item')
-        .html('<a href="'+this.items[i]+'.html">'+this.items[i]+'</a>')
-      );
+        // ehh, there is probably a better way to do this
+        .html(
+          '<a href="' + this.items[this.items_order[i]].link + '">' +
+          this.items[this.items_order[i]].name + '</a>'
+        )
+    );
   }
 
   this.mask = $(document.createElement('div'))
