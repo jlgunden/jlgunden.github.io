c6f07046f77e5a1bd58a0eb8e30b9229e426d39e
jlgunden <jlgunden@gmail.com>
2015-08-31 14:54:21 -0400

    changing text and some clean up

--- js/history_manager.js
+++ js/history_manager.js
@@ -55,7 +55,7 @@ history_manager.load_handler = function(href, opt_update_history) {
   var title = (href === '/') ? 'Jacob Gunden' : 'Jacob Gunden | ' + href;
   document.title = title;
 
-  // call the page's init function if the new state is not the initial page
+  // call the page's init function if it exists as a function
   var init_method = (href === '/') ? 'index' : href;
   if (typeof(initializer[init_method]) === 'function') {
     initializer[init_method]();
@@ -80,78 +80,5 @@ window.addEventListener('popstate', function(e) {
       $(children[i]).addClass('active');
       break;
     }
-
   }
 });
-
-
-// var change_state = function(e) {
-//   var url = $(e.target).attr('href');
-
-//   $('#layer').load(url + ' #layer > *', function() {
-
-//     // update desktop navigation title classes
-//     $('.navigation_title.active').removeClass('active');
-//     var children = $('.nav_desktop').children();
-//     for (var i = 0; i < children.length; i++) {
-//       var data = $(children[i]).data();
-//       if (data.path === url) {
-//         $(children[i]).addClass('active');
-//         break;
-//       }
-//     }
-
-//     if (url !== window.location.pathname) {
-//       history.pushState(url, null, url);
-//       var title = (url === '/') ? 'Jacob Gunden' : 'Jacob Gunden | ' + url;
-//       document.title = title;
-//     }
-
-//     window.scrollTo(0, 0);
-
-//     // call page's init function
-//     if (url !== default_state) {
-//       var init_method = (url === '/') ? 'index': url;
-//       if (typeof(initializer[init_method]) === 'function') {
-//         initializer[init_method]();
-//       }
-//     }
-//   });
-// };
-
-// var default_state = window.location.pathname;
-// var default_title = document.title;
-// window.addEventListener('popstate', function(e) {
-//   var state = e.state;
-
-//   if (state !== null) {
-//     $('#layer').load(state + ' #layer > *', function() {
-//       var title = (state === '/' || state === "") ? 'Jacob Gunden' : 'Jacob Gunden | ' + state;
-//       document.title = title;
-//     });
-//   }
-//   else {
-//     $('#layer').load(default_state + ' #layer > *', function() {
-//       document.title = default_title;
-//     });
-//   }
-
-//   // update title active class
-//   $('.navigation_title.active').removeClass('active');
-//   var children = $('.nav_desktop').children();
-//   for (var i = 0; i < children.length; i++) {
-//     var data = $(children[i]).data();
-//     if (state === null) {
-//       if (data.path === default_state) {
-//         $(children[i]).addClass('active');
-//         break;
-//       }
-//     }
-//     else {
-//       if (data.path === state) {
-//         $(children[i]).addClass('active');
-//         break;
-//       }
-//     }
-//   }
-// });
